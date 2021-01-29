d333b99afcc0b660403ccb3a286b0debbbf01af6
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-29 15:00:12 -0500

    working on the data list writing to dom

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -14,7 +14,36 @@ const journal = [
       concept: "HTML & CSS",
       entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
       mood: "Ok"
+  },
+  {
+    id: 2,
+    date: "07/26/2025",
+    concept: "Complex Flexbox",
+    entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+    mood: "Sad"
+  },
+  {
+    id: 3,
+    date: "07/27/2025",
+    concept: "Adding a Functional Associates Button",
+    entry: "This time I had to add a button to each criminal that makes their known associates and alibis appear somewhere on the screen.",
+    mood: "Neutral"
+  },
+  {
+    id: 4,
+    date: "07/28/2025",
+    concept: "Filtering Data",
+    entry: "I had to set up a dropdown so that it only returned criminal objects that matched the value of what the user selects from the list.",
+    mood: "Neutral"
+  },
+  {
+    id: 5,
+    date: "07/29/2025",
+    concept: "Printing to the DOM",
+    entry: "It's hard to think about all the different components that need to communicate with each other.",
+    mood: "Neutral"
   }
+
 ]
 
 /*
