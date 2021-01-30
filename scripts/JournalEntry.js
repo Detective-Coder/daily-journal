9083f6dd59ccc7f96b63e3d8ca3874549e973a18
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-30 09:41:05 -0500

    styling the list components printing to the dom

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -4,9 +4,11 @@
  */
 export const JournalEntryComponent = (entry) => {
   return `
-      <section id="entry--${entry.id}" class="journalEntry">
+      <section id="entry--${entry.id}" class="journalEntry bg-light">
+        <div class="border rounded overflow-hidden">
           <p>${entry.entry}</p>
           <p>${entry.date}</p>
+        </div>
       </section>
   `
 }
