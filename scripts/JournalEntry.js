aa418fa8656b79dca64a78d280aae2729eb19534
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-31 13:06:14 -0500

    styling the journal entries printing to the dom

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -4,11 +4,14 @@
  */
 export const JournalEntryComponent = (entry) => {
   return `
-      <section id="entry--${entry.id}" class="journalEntry bg-light">
-        <div class="border rounded overflow-hidden">
+    <div class="col-sm bg-light m-2">
+      <section id="entry--${entry.id}" class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
           <p>${entry.entry}</p>
           <p>${entry.date}</p>
         </div>
       </section>
+    </div>
+  
   `
 }
