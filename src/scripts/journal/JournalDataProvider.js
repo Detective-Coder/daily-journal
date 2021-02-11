e4eb0fe9fae1a43a414c7463a1893d3b0fcfe84d
Detective-Coder <aaronfrankenfield@gmail.com>
2021-02-11 09:45:58 -0500

    added parens to methods and now everything works

--- src/scripts/journal/JournalDataProvider.js
+++ src/scripts/journal/JournalDataProvider.js
@@ -1,50 +1,3 @@
-/*
- *   Journal data provider for Daily Journal application
- *
- *      Holds the raw data about each entry and exports
- *      functions that other modules can use to filter
- *      the entries for different purposes.
- */
-
-// This is the original data.
-// const journal = [
-//   {
-//       id: 1,
-//       date: "07/24/2025",
-//       concept: "HTML & CSS",
-//       entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-//       mood: "Neutral"
-//   },
-//   {
-//       id: 2,
-//       date: "07/24/2025",
-//       concept: "HTML & CSS",
-//       entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-//       mood: "Ok"
-//   },
-//   {
-//       id: 3,
-//       date: "01/14/2021",
-//       concept: "HTML & JavaScript",
-//       entry: "We talked about components and creating objects.",
-//       mood: "Neutral"
-//   },
-//   {
-//       id: 4,
-//       date: "01/15/2021",
-//       concept: "HTML & JavaScript",
-//       entry: "We learned about using JavaScript to print to the DOM.",
-//       mood: "Happy"
-//   },
-//   {
-//       id: 5,
-//       date: "01/19/2021",
-//       concept: "HTML & JavaScript",
-//       entry: "We learned about using JavaScript to print HTML snippets to the DOM.",
-//       mood: "Happy"
-//   }
-// ];
-
 let entries = []
 /*
   You export a function that provides a version of the
@@ -57,12 +10,12 @@ let entries = []
 //   )
 //   return sortedByDate
 // }
-export const useJournalEntries = () => entries.slice
+export const useJournalEntries = () => entries.slice()
 
 export const getEntries = () => {
   return fetch("http://localhost:8088/entries")
-    .then(response => response.json)
-    .then((parsedEntries) => {
+    .then(response => response.json())
+    .then(parsedEntries => {
       entries = parsedEntries
     })
 }
@@ -75,4 +28,5 @@ export const saveEntry = entry => {
     },
     body: JSON.stringify(entry)
   })
+  .then(getEntries)
 }
--- src/scripts/journal/JournalEntryList.js
+++ src/scripts/journal/JournalEntryList.js
@@ -1,9 +1,3 @@
-/*
- *  Purpose:
- *    To render as many journal entry components as
- *    there are items in the collection exported by the
- *    data provider component
- */
 import { useJournalEntries, getEntries } from "./JournalDataProvider.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
@@ -14,8 +8,9 @@ export const EntryListComponent = () => {
   entryLog.innerHTML = "";
   getEntries().then(() => {
     // Use the journal entry data from the data provider component
-    let entries = useJournalEntries()
-    entries.forEach((singleEntry) => {
+    let journalEntries = useJournalEntries()
+    debugger   
+    journalEntries.map((singleEntry) => {
       entryLog.innerHTML += JournalEntryComponent(singleEntry);
     });
   })
