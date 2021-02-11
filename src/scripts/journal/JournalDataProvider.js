caa7e9da83911636f0a8c63afcdb71d2ad93ac18
Detective-Coder <aaronfrankenfield@gmail.com>
2021-02-11 17:14:38 -0500

    commenting code

--- src/scripts/journal/JournalDataProvider.js
+++ src/scripts/journal/JournalDataProvider.js
@@ -1,33 +1,23 @@
+// empty array to store the entries we get back from the api fetch call
 let entries = []
-/*
-  You export a function that provides a version of the
-  raw data in the format that you want
-*/
-// export const useJournalEntries = () => {
-//   const sortedByDate = journal.sort(
-//       (currentEntry, nextEntry) =>
-//           Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-//   )
-//   return sortedByDate
-// }
-
+// the function we call to use the journal entries in another module
 export const useJournalEntries = () => entries.slice()
-
+// the function that gets the entries from the api
 export const getEntries = () => {
-  return fetch("http://localhost:8088/entries?_expand=mood")
-    .then(response => response.json())
-    .then(parsedEntries => {
-      entries = parsedEntries
+  return fetch("http://localhost:8088/entries?_expand=mood") //get the entries
+    .then(response => response.json()) //turn them into json we can use
+    .then(parsedEntries => { 
+      entries = parsedEntries //store the results in the global entries variable
     })
 }
-
+// the function we call to save a new entry to the api
 export const saveEntry = entry => {
-  return fetch("http://localhost:8088/entries?_expand=mood", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
+  return fetch("http://localhost:8088/entries?_expand=mood", { //connect to the api
+    method: "POST", //use post instead of get to post a new entry to the api
+    headers: { 
+      "Content-Type": "application/json" //required for some reason
     },
-    body: JSON.stringify(entry)
+    body: JSON.stringify(entry) //required
   })
-  .then(getEntries)
+  .then(getEntries) //run the getEntries function again so we refresh them with our new entry added
 }
